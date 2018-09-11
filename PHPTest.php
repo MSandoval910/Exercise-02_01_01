7a312bb00e4a96eea5f56b17ff208542d70e6df6
Unknown <MSandoval910@west-mec.org>
2018-09-10 17:59:18 -0700

    commit
    added two new files and added some test php into the HTML to see if it works correctly.

---
+++ PHPTest.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<!--
+   Exercise 02_01_01
+    Author: Mario Sandoval
+    Date: 10 September 2018
+    
+    PHPCodeBlocks.php
+-->
+
+<html>
+
+<head>
+    <title>PHP Code Blocks</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0">
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+
+<body>
+      
+    <?php
+        phpinfo();
+    ?>
+</body>
+
+</html>
